102aacafc6b8d106966ee8d0143797fa2f2110be
Gilad Gurantz <ggurantz@gmail.com>
2016-12-09 12:47:15 -0800

    Added option to launch Lyft in Safari without leaving the app (#4)
    Created the concept of "deep link behavior" to specify between launching Lyft's mobile web experience and launching the native Lyft app

--- Sources/LyftUI/LyftDeepLink.swift
+++ Sources/LyftUI/LyftDeepLink.swift
@@ -2,57 +2,90 @@ import Foundation
 import CoreLocation
 import UIKit
 
+/// Designates the kind of deeplinking to perform
+///
+/// - native:   Launches the native Lyft app if available
+/// - web:      Launches a safari view controller without leaving the app, enabling ride request function
+public enum LyftDeepLinkBehavior {
+    case native
+    case web
+
+    fileprivate var baseUrl: URL? {
+        switch self {
+            case .native:
+                return URL(string: "lyft://")
+
+            case .web:
+                return URL(string: "https://ride.lyft.com/")
+        }
+    }
+}
+
 /// Collection of deep links into the main Lyft application
 public struct LyftDeepLink {
+
     /// Prepares to request a ride with the given parameters
     ///
-    /// - parameter kind:                   The kind of ride to create a request for
-    /// - parameter pickup:                 The pickup position of the ride
-    /// - parameter destination:            The destination position of the ride
-    /// - parameter couponCode              A coupon code to be applied to the user
-    public static func requestRide(kind: RideKind,
+    /// - parameter behavior:       The deep linking mode to use for this deep link
+    /// - parameter kind:           The kind of ride to create a request for
+    /// - parameter pickup:         The pickup position of the ride
+    /// - parameter destination:    The destination position of the ride
+    /// - parameter couponCode:     A coupon code to be applied to the user
+    public static func requestRide(using behavior: LyftDeepLinkBehavior = .native, kind: RideKind = .Standard,
                                    from pickup: CLLocationCoordinate2D? = nil,
                                    to destination: CLLocationCoordinate2D? = nil,
                                    couponCode: String? = nil)
     {
-        var parameters = ["id": kind.rawValue]
-
-        if let pickup = pickup {
-            parameters["pickup[latitude]"] = String(pickup.latitude)
-            parameters["pickup[longitude]"] = String(pickup.longitude)
-        }
-
-        if let destination = destination {
-            parameters["destination[latitude]"] = String(destination.latitude)
-            parameters["destination[longitude]"] = String(destination.longitude)
-        }
-
+        let action: String
+        var parameters = [String: Any]()
         parameters["partner"] = LyftConfiguration.developer?.clientId
         parameters["credits"] = couponCode
 
-        self.launch(action: "ridetype", parameters: parameters)
-    }
+        switch behavior {
+            case .native:
+                action = "ridetype"
+                parameters["id"] = kind.rawValue
+                parameters["pickup[latitude]"] = pickup.map { $0.latitude }
+                parameters["pickup[longitude]"] = pickup.map { $0.longitude }
+                parameters["destination[latitude]"] = destination.map { $0.latitude }
+                parameters["destination[longitude]"] = destination.map { $0.longitude }
 
-    private static func launch(action: String, parameters: [String: String]?) {
-        var requestURLComponents = URLComponents()
-        requestURLComponents.scheme = "lyft"
-        requestURLComponents.host = action
-        requestURLComponents.queryItems = parameters?.map { key, value in
-            URLQueryItem(name: key, value: value)
+            case .web:
+                action = "request"
+                parameters["ride_type"] = kind.rawValue
+                parameters["pickup"] = pickup.map { "@\($0.latitude),\($0.longitude)" }
+                parameters["destination"] = destination.map { "@\($0.latitude),\($0.longitude)" }
         }
 
-        guard let url = requestURLComponents.url else {
+        self.launch(using: behavior, action: action, parameters: parameters)
+    }
+
+    private static func launch(using behavior: LyftDeepLinkBehavior, action: String,
+                               parameters: [String: Any])
+    {
+        guard let originalUrl = behavior.baseUrl.flatMap({ URL(string: action, relativeTo: $0) }) else {
             return
         }
 
-        if #available(iOS 10.0, *) {
-            UIApplication.shared.open(url, options: [:]) { success in
-                if !success {
-                    self.launchAppStore()
+        let request = lyftURLEncodedInURL(request: URLRequest(url: originalUrl), parameters: parameters).0
+        guard let url = request.url else {
+            return
+        }
+
+        switch behavior {
+            case .native:
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(url, options: [:]) { success in
+                        if !success {
+                            self.launchAppStore()
+                        }
+                    }
+                } else {
+                    UIApplication.shared.openURL(url)
                 }
-            }
-        } else {
-            UIApplication.shared.openURL(url)
+
+            case .web:
+                Safari.openURL(url, from: UIApplication.shared.topViewController)
         }
     }
 
@@ -66,5 +99,16 @@ public struct LyftDeepLink {
         if let signUpUrl = URL(string: url) {
             UIApplication.shared.open(signUpUrl, options: [:], completionHandler: nil)
         }
+    }
+}
+
+fileprivate extension UIApplication {
+    fileprivate var topViewController: UIViewController? {
+        var topController = self.keyWindow?.rootViewController
+        while let viewController = topController?.presentedViewController {
+            topController = viewController
+        }
+
+        return topController
     }
 }
